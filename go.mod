9f8d0d03f734b02ed352bea6e2a120117a3ed7d3
Rudianto Tiofan <rudianto.dev@gmail.com>
2024-03-06 05:52:06 +0700

    setup user migration

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/rudianto-dev/go-boilerplate-migration
+
+go 1.22.0
+
+require github.com/pressly/goose/v3 v3.18.0
+
+require (
+	github.com/mfridman/interpolate v0.0.2 // indirect
+	github.com/sethvargo/go-retry v0.2.4 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/sync v0.6.0 // indirect
+)
---
+++ user/20240305224850_create_user_table.go
@@ -0,0 +1,38 @@
+package user
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/pressly/goose/v3"
+)
+
+func init() {
+	goose.AddMigrationContext(upCreateUserTable, downCreateUserTable)
+}
+
+func upCreateUserTable(ctx context.Context, tx *sql.Tx) error {
+	_, err := tx.Exec(`
+		CREATE TABLE IF NOT EXISTS users
+		(
+			id CHARACTER VARYING(30) NOT NULL,
+			name VARCHAR(255) NOT NULL,
+			created_at BIGINT NOT NULL DEFAULT 0,
+			updated_at BIGINT NOT NULL DEFAULT 0,
+
+			PRIMARY KEY (id)
+		);
+	`)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func downCreateUserTable(ctx context.Context, tx *sql.Tx) error {
+	_, err := tx.Exec("DROP TABLE IF EXISTS users;")
+	if err != nil {
+		return err
+	}
+	return nil
+}
